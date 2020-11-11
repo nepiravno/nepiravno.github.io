7c6fb4b9f34f88af6e5a55097aa05dd12970be2b
Grigorij Livada <nepiravno@gmail.com>
2020-11-11 23:25:16 +0200

    back to js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// console.log('Hello!');
+'use strict'
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: 'My menu',
+};
+
+multiplyNumeric(menu);
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
+    }
+}
+
+console.log(menu);
