a3762a5e55ebb53d3cbe2a2c5541c0b3fc3c7cda
Grigorij Livada <nepiravno@gmail.com>
2020-11-30 19:52:58 +0200

    func filterRange

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-let camelize = (str) => {
-  return str
-  .split('-')
-  .map((word, index) => 
-  index == 0 ? word : word[0].toUpperCase() + word.slice(1))
-  .join('');
+let arr = [5, 3, 8, 1];
+
+let filterRange = (arr, a, b) => {
+  return arr.filter( item => 
+         a <= item && item < b);
 }
 
-console.log( camelize("background-color") ); 
-console.log( camelize("list-style-image") );
-console.log( camelize("-webkit-transition") );
+let filtered = filterRange(arr, 1, 4);
+
+console.log(filtered);
+console.log(arr);
