3462778736ed900819adcc6f028eb6bb1632867e
Grigorij Livada <grigorij.livada@gmail.com>
2021-02-08 18:42:33 +0200

    some commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,18 @@
 'use strict';
 
-let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-
-let aclean = arr => {
-  // let set = new Set();
-  // let currentSize = 0;
-  // let resultArr = [];
-
-  // for ( let item of arr ) {
-  //   currentSize = set.size;
-  //   set.add( item.split('').sort().join('').toLowerCase() );
-
-  //   if ( set.size > currentSize ) {
-  //     resultArr.push(item);
-  //   }
-  // }
-
-  // return resultArr;
-  let map = new Map();
-
-  for ( let item of arr ) {
-    let sorted = item.toLowerCase().split('').sort().join('');
-    map.set(sorted, item);
-  }
-
-  return Array.from(map.values());
+let user = {
+  name: 'John',
+  age: 30
 };
 
+let count = obj => {
+  let count = 0;
 
+  for ( let key in obj ) {
+    count += 1;
+  }
 
-alert( aclean(arr) ); 
-// "nap,teachers,ear" or "PAN,cheaters,era"
+  return count;
+}
+
+alert( count(user) ); // 2
